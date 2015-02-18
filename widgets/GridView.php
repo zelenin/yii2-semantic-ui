4ed55365e2624150a2d3624382c45b886606cfaa
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-02-18 12:10:06 +0300

    change default grid table

--- widgets/GridView.php
+++ widgets/GridView.php
@@ -4,7 +4,7 @@ namespace Zelenin\yii\SemanticUI\widgets;
 
 class GridView extends \yii\grid\GridView
 {
-    public $tableOptions = ['class' => 'ui very basic table'];
+    public $tableOptions = ['class' => 'ui table'];
     public $dataColumnClass = 'Zelenin\yii\SemanticUI\widgets\DataColumn';
     public $pager = ['class' => 'Zelenin\yii\SemanticUI\widgets\LinkPager'];
 }
