09b75ef20e8fd18f84a2ff74c0dc799ddfb25f99
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-07-05 16:21:04 +0300

    add sizes

--- helpers/Size.php
+++ helpers/Size.php
@@ -4,9 +4,12 @@ namespace Zelenin\yii\SemanticUI\helpers;
 
 class Size
 {
-    const HUGE = 'huge';
-    const LARGE = 'large';
-    const MEDIUM = 'medium';
-    const SMALL = 'small';
+    const MINI = 'mini';
     const TINY = 'tiny';
+    const SMALL = 'small';
+    const MEDIUM = 'medium';
+    const LARGE = 'large';
+    const BIG = 'big';
+    const HUGE = 'huge';
+    const MASSIVE = 'massive';
 }
