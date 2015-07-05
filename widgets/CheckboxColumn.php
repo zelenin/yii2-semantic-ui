e6b81d7e38fa1bd13caa1ca4d6b6ce3646147be2
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-07-05 21:00:10 +0300

    checkboxcolumn refactoring

--- widgets/CheckboxColumn.php
+++ widgets/CheckboxColumn.php
@@ -5,6 +5,7 @@ namespace Zelenin\yii\SemanticUI\widgets;
 use Closure;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
+use yii\web\JsExpression;
 use Zelenin\yii\SemanticUI\modules\Checkbox;
 
 class CheckboxColumn extends \yii\grid\CheckboxColumn
@@ -12,13 +13,6 @@ class CheckboxColumn extends \yii\grid\CheckboxColumn
     protected function renderHeaderCellContent()
     {
         $name = rtrim($this->name, '[]') . '_all';
-        $id = $this->grid->options['id'];
-        $options = Json::encode([
-            'name' => $this->name,
-            'multiple' => $this->multiple,
-            'checkAll' => $name,
-        ]);
-        $this->grid->getView()->registerJs("jQuery('#$id').yiiGridView('setSelectionColumn', $options);");
 
         if ($this->header !== null || !$this->multiple) {
             return parent::renderHeaderCellContent();
@@ -28,6 +22,16 @@ class CheckboxColumn extends \yii\grid\CheckboxColumn
                 'checked' => false,
                 'inputOptions' => [
                     'class' => 'select-on-check-all'
+                ],
+                'clientOptions' => [
+                    'onChecked' => new JsExpression('function() {
+                        var $childCheckbox = $(this).closest("#' . $this->grid->options['id'] .'").find("input[name=\'' . $this->name . '\']").closest(".checkbox");
+                        $childCheckbox.checkbox("check");
+                    }'),
+                    'onUnchecked' => new JsExpression('function() {
+                        var $childCheckbox = $(this).closest("#' . $this->grid->options['id'] .'").find("input[name=\'' . $this->name . '\']").closest(".checkbox");
+                        $childCheckbox.checkbox("uncheck");
+                    }')
                 ]
             ]);
         }
@@ -47,7 +51,32 @@ class CheckboxColumn extends \yii\grid\CheckboxColumn
         return Checkbox::widget([
             'name' => $this->name,
             'checked' => !empty($options['checked']),
-            'inputOptions' => $options
+            'inputOptions' => $options,
+            'clientOptions' => [
+                'fireOnInit' => true,
+                'onChange' => new JsExpression('function() {
+                    var $parentCheckbox = $(".select-on-check-all").closest(".checkbox"),
+                    $checkbox = $("#' . $this->grid->options['id'] .'").find("input[name=\'' . $this->name . '\']").closest(".checkbox"),
+                    allChecked = true,
+                    allUnchecked = true;
+
+                    $checkbox.each(function() {
+                        if ($(this).checkbox("is checked")) {
+                            allUnchecked = false;
+                        } else {
+                            allChecked = false;
+                        }
+                    });
+
+                    if(allChecked) {
+                        $parentCheckbox.checkbox("set checked");
+                    } else if(allUnchecked) {
+                        $parentCheckbox.checkbox("set unchecked");
+                    } else {
+                        $parentCheckbox.checkbox("set indeterminate");
+                    }
+                }'),
+            ]
         ]);
     }
 }
--- widgets/GridView.php
+++ widgets/GridView.php
@@ -4,7 +4,18 @@ namespace Zelenin\yii\SemanticUI\widgets;
 
 class GridView extends \yii\grid\GridView
 {
+    /**
+     * @var array
+     */
     public $tableOptions = ['class' => 'ui table'];
+
+    /**
+     * @var string
+     */
     public $dataColumnClass = 'Zelenin\yii\SemanticUI\widgets\DataColumn';
+
+    /**
+     * @var array
+     */
     public $pager = ['class' => 'Zelenin\yii\SemanticUI\widgets\LinkPager'];
 }
