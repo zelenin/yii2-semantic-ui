4c915382619d7c4e21594d1f390471a0092edaa3
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-07-05 15:33:01 +0300

    add color helper

---
+++ helpers/Color.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Zelenin\yii\SemanticUI\helpers;
+
+class Color
+{
+    const RED = 'red';
+    const ORANGE = 'orange';
+    const YELLOW = 'yellow';
+    const OLIVE = 'olive';
+    const GREEN = 'green';
+    const TEAL = 'teal';
+    const BLUE = 'blue';
+    const VIOLET = 'violet';
+    const PURPLE = 'purple';
+    const PINK = 'pink';
+    const BROWN = 'brown';
+    const GREY = 'grey';
+    const BLACK = 'black';
+}
