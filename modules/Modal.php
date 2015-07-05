00e93cf8346f856ef9be8df61d924253188492a9
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-07-05 21:56:31 +0300

    modal phpdocs

--- modules/Modal.php
+++ modules/Modal.php
@@ -10,27 +10,55 @@ use Zelenin\yii\SemanticUI\Widget;
 
 class Modal extends Widget
 {
+    /**
+     * @var string
+     */
     public $closeButton = '<i class="close icon"></i>';
 
+    /**
+     * @var string
+     */
     public $header;
+    /**
+     * @var array
+     */
     public $headerOptions = [];
 
+    /**
+     * @var string
+     */
     public $content;
+    /**
+     * @var array
+     */
     public $contentOptions = [];
 
+    /**
+     * @var string
+     */
     public $actions;
+    /**
+     * @var array
+     */
     public $actionsOptions = [];
 
+    /**
+     * @var string
+     */
     public $type = self::TYPE_STANDARD;
     const TYPE_BASIC = 'basic';
     const TYPE_STANDARD = 'standard';
 
+    /**
+     * @var bool
+     */
     public $fullscreen = false;
     const TYPE_FULLSCREEN = 'fullscreen';
 
+    /**
+     * @var string
+     */
     public $size;
-    const SIZE_LARGE = 'large';
-    const SIZE_SMALL = 'small';
 
     public function init()
     {
@@ -60,23 +88,38 @@ class Modal extends Widget
         echo Html::endTag('div');
     }
 
+    /**
+     * @return string
+     */
     public function renderCloseButton()
     {
         return $this->closeButton;
     }
 
+    /**
+     * @return null|string
+     */
     public function renderHeader()
     {
         Html::addCssClass($this->headerOptions, 'header');
         return $this->header ? Html::tag('div', $this->header, $this->headerOptions) : null;
     }
 
+    /**
+     * @return null|string
+     */
     public function renderActions()
     {
         Html::addCssClass($this->actionsOptions, 'actions');
         return $this->actions ? Html::tag('div', $this->actions, $this->actionsOptions) : null;
     }
 
+    /**
+     * @param string $content
+     * @param array $options
+     *
+     * @return string
+     */
     public function renderToggleButton($content, $options = [])
     {
         if (!isset($options['id'])) {
