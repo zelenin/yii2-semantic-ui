d859664aaa4416896dd33121be39d694492945de
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-07-17 13:09:25 +0300

    change buttons in ActionColumn

--- widgets/ActionColumn.php
+++ widgets/ActionColumn.php
@@ -8,11 +8,17 @@ use Zelenin\yii\SemanticUI\Elements;
 
 class ActionColumn extends \yii\grid\ActionColumn
 {
+    /**
+     * @var string
+     */
+    public $template = '<div class="ui basic tiny compact icon buttons">{view}{update}{delete}</div>';
+
     protected function initDefaultButtons()
     {
         if (!isset($this->buttons['view'])) {
             $this->buttons['view'] = function ($url, $model, $key) {
                 return Html::a(Elements::icon('eye'), $url, [
+                    'class' => 'ui button',
                     'title' => Yii::t('yii', 'View'),
                     'data-pjax' => '0',
                 ]);
@@ -21,6 +27,7 @@ class ActionColumn extends \yii\grid\ActionColumn
         if (!isset($this->buttons['update'])) {
             $this->buttons['update'] = function ($url, $model, $key) {
                 return Html::a(Elements::icon('pencil'), $url, [
+                    'class' => 'ui button',
                     'title' => Yii::t('yii', 'Update'),
                     'data-pjax' => '0',
                 ]);
@@ -29,6 +36,7 @@ class ActionColumn extends \yii\grid\ActionColumn
         if (!isset($this->buttons['delete'])) {
             $this->buttons['delete'] = function ($url, $model, $key) {
                 return Html::a(Elements::icon('trash'), $url, [
+                    'class' => 'ui button',
                     'title' => Yii::t('yii', 'Delete'),
                     'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
                     'data-method' => 'post',
