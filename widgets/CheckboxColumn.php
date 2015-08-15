0afa5b3926a6aa9bcbca2f1303a4a2250a2b9c53
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-08-16 02:36:22 +0300

    checkbox column fix

--- widgets/CheckboxColumn.php
+++ widgets/CheckboxColumn.php
@@ -19,6 +19,13 @@ class CheckboxColumn extends \yii\grid\CheckboxColumn
     protected function renderHeaderCellContent()
     {
         $name = rtrim($this->name, '[]') . '_all';
+        $id = $this->grid->options['id'];
+        $options = json_encode([
+            'name' => $this->name,
+            'multiple' => $this->multiple,
+            'checkAll' => $name,
+        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        $this->grid->getView()->registerJs("jQuery('#$id').yiiGridView('setSelectionColumn', $options);");
 
         if ($this->header !== null || !$this->multiple) {
             return parent::renderHeaderCellContent();
