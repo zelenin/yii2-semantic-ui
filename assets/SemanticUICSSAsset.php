f7d0f78f59d58bdcbd238a3237180dd02f153db5
Derrick Egersdorfer <hello@codechap.com>
2018-01-03 06:41:18 +0200

    Use bower alias
    The directory of bower has changed and breaks the installation of Semantic, the alias is needed to correct it.

--- assets/SemanticUICSSAsset.php
+++ assets/SemanticUICSSAsset.php
@@ -10,7 +10,7 @@ class SemanticUICSSAsset extends AssetBundle
     /**
      * @var string
      */
-    public $sourcePath = '@vendor/bower/semantic/dist';
+    public $sourcePath = '@bower/semantic/dist';
 
     public function init()
     {
